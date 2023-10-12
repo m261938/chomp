5e7a0f9cfbaac5f7f5146b63afea9336e3027473
m261938 <m261938@usna.edu>
2023-10-12 09:44:40 -0400

    background and fish box

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
-print('hello world!')
-print('new message')
+import pygame
+import time
+
+print(f'the quit event is type {pygame.QUIT}')
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("Chomp!")
+screen.fill((101,178,255))
+pygame.draw.rect(screen,(246,230,178),(0,380,400,400))
+pygame.draw.rect(screen,(0,255,0),(200,200,40,20))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print('not today! this game runs forrrreverrrr')

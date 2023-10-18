5de5d4abd1b9456e3ebc7cc28c65ca08f2367b2f
m261938 <m261938@usna.edu>
2023-10-18 09:27:38 -0400

    use parametric variables

--- game.py
+++ game.py
@@ -1,18 +1,30 @@
 import pygame
-import time
+import sys
 
-print(f'the quit event is type {pygame.QUIT}')
+# dimensions
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 500
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tiles are square height == width
+
+# colors
+WATER_COLOR = (101, 178, 255)
+SAND_COLOR = (246, 230, 178)
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((101,178,255))
-pygame.draw.rect(screen,(246,230,178),(0,380,400,400))
-pygame.draw.rect(screen,(0,255,0),(200,200,40,20))
+screen.fill(WATER_COLOR)
 
+pygame.draw.rect(screen, SAND_COLOR, (0, SCREEN_HEIGHT - SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))  # sand
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand, (SCREEN_WIDTH / 2 - TILE_SIZE / 2, SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
 pygame.display.flip()
 
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            print('not today! this game runs forrrreverrrr')
+            print('thanks for playing!')
+            pygame.quit()
+            sys.exit()
